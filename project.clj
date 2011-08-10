0c8fa461a8e418ea4ae07382058429df2e233d59
Stefan Kamphausen <ska2342@googlemail.com>
2011-08-10 23:57:49 +0200

    added :url, :license

--- project.clj
+++ project.clj
@@ -1,6 +1,11 @@
 (defproject adamant-run "1.0.0-SNAPSHOT"
   :description "A Clojure library to adamantly (or stubbornly) try to run a function "
-  :dependencies [[org.clojure/clojure "1.2.1"]]
+
+  :url "https://github.com/ska2342/adamant-run"
+  :license {:name "Eclipse Public License"}
+  
+  :dependencies     [[org.clojure/clojure "1.2.1"]]
   :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]]
+
   :aot [adamant-run.AdamantRetryException]
   )
